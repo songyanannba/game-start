ec03a9e9db7d7a5f97d99a04b1741148b76bf408
songyanannba <1499067030@qq.com>
2023-07-05 16:19:39 +0800

    feat:slot 6 succ

--- slots/slotClient/hander/slotHander.go
+++ slots/slotClient/hander/slotHander.go
@@ -38,3 +38,65 @@ func SlotTest(c *gin.Context) {
 		"data":  "xxxxxxxxttt",
 	})
 }
+
+func Slot6Spin(c *gin.Context) {
+
+	//grpc 请求
+	conn, err := grpc.Dial("192.168.6.119:9001", grpc.WithInsecure())
+	if err != nil {
+		fmt.Println("slit test grpc dial err", err)
+		return
+	}
+
+	client := pb.NewSlotServiceClient(conn)
+
+	slot6Spin, err := client.SlotSpin(context.Background(), &pb.SpinReq{
+		GameId:    6,
+		SessionId: 0,
+		Uid:       1,
+		FreeNum:   1,
+		ResNum:    1,
+		Raise:     1,
+		Bet:       100,
+	})
+	if err != nil {
+		fmt.Println("client.slot6Spin err", err)
+		return
+	}
+
+	fmt.Println("client slot6Spin  === ", slot6Spin)
+
+	c.JSON(http.StatusOK, gin.H{
+		"mag":   "ok",
+		"total": "1001",
+		"data":  "xxxxxxxxttt",
+	})
+}
+
+func Slot6SpinTest() {
+
+	//grpc 请求
+	conn, err := grpc.Dial("192.168.6.119:9001", grpc.WithInsecure())
+	if err != nil {
+		fmt.Println("slit test grpc dial err", err)
+		return
+	}
+
+	client := pb.NewSlotServiceClient(conn)
+
+	_, err = client.SlotSpin(context.Background(), &pb.SpinReq{
+		GameId:    6,
+		SessionId: 0,
+		Uid:       1,
+		FreeNum:   1,
+		ResNum:    1,
+		Raise:     1,
+		Bet:       100,
+	})
+	if err != nil {
+		fmt.Println("client.slot6Spin err", err)
+		return
+	}
+
+	//fmt.Println("client slot6Spin  === ", slot6Spin)
+}
--- slots/slotClient/main.go
+++ slots/slotClient/main.go
@@ -12,6 +12,7 @@ func main() {
 	accountGroup := r.Group("/v1/slot")
 	{
 		accountGroup.GET("/SlotTest", hander.SlotTest)
+		accountGroup.GET("/Slot6Spin", hander.Slot6Spin)
 	}
 	r.Run(fmt.Sprintf("%s:%d", "192.168.6.119", 9002))
 }
